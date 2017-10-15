7b6e11f985413fe3d2cf54b4b217165ec65babcf
yves songolo <yves.songolo@gmail.com>
2017-10-14 22:38:33 -0700

    setup environment

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, make_response
+from flask_restful import Resource, Api
+from pymongo import MongoClient
+from utils.mongo_json_encoder import JSONEncoder
+from bson.objectid import ObjectId
+import bcrypt
+from mongoengine import *
+import pdb
+import uuid
+from socket import *
+from basicauth import decode
